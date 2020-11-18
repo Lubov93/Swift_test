157a16887abac64d748bef51993ce9f8f08bc006
Lubov93 <ru.lubov@gmail.com>
2020-11-18 11:38:03 +0200

    Created test project on Swift

---
+++ NewOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewOne
+//
+//  Created by Lubanya  on 28.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "", on: sender)
+        
+    }
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+}
+
+}
